57c7262318cb90b2656beb71596bb066b865d5fb
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-21 17:45:57 -0400

    error fixing in regex

--- scripts/polling.js
+++ scripts/polling.js
@@ -6,6 +6,9 @@
     var Auth = require('../lib/auth.js');
     var auth = new Auth();
 
+    var durationMin = 1;
+    var durationMax = 60;
+
     robot.hear(reg.exp('!(rtv|vote)( \\+(\\d+))? (.*) (\\\\.*)+'), function(res) {
       var duration = 10;
       if (res.match[4]) {
@@ -13,9 +16,20 @@
       }
       var question = res.match[5];
       var choices = res.match[6].split('\\');
-      console.log(duration);
-      console.log(question);
-      console.log(choices);
+      var empty = /^\s*$/;
+      for (var i = 0; i < choices.length; i++) {
+        if (empty.test(choices[i])) {
+          choices.splice(i--, 1);
+        }
+      }
+      if (choices.length < 2) {
+        return res.reply(':reject: need to vote on at least one thing, yo');
+      }
+      if (duration < 1 || duration > 60) {
+        return res.reply(
+          ':reject: the min duration is ' + durationMin.toString() + ' and the max duration is ' + durationMax.toString()
+        );
+      }
     });
 
   }
