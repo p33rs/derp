84ec4387f31cca402f9870d3db171072191aec1d
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-21 17:21:15 -0400

    finished regex

--- scripts/polling.js
+++ scripts/polling.js
@@ -6,8 +6,16 @@
     var Auth = require('../lib/auth.js');
     var auth = new Auth();
 
-    robot.hear(reg.exp('!(rtv|vote) (+(\\d+)) (.*) (\\\.*)+'), function(res) {
-      console.log(res.match);
+    robot.hear(reg.exp('!(rtv|vote)( \\+(\\d+))? (.*) (\\\\.*)+'), function(res) {
+      var duration = 10;
+      if (res.match[4]) {
+        duration = res.match[4];
+      }
+      var question = res.match[5];
+      var choices = res.match[6].split('\\');
+      console.log(duration);
+      console.log(question);
+      console.log(choices);
     });
 
   }
