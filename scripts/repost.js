b02bc4da3a2d645de4d49a1ad4e157d1d14967be
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-22 01:19:18 -0400

    reposter

---
+++ scripts/repost.js
@@ -0,0 +1,44 @@
+(function() {
+  module.exports = function(robot) {
+    var fs = require ('fs');
+    var jsonfile = require('jsonfile');
+    var filename = '/home/jon/hubot/storage/repost.json';
+
+    robot.hear(/https?:\/\/\S+/i, function(res) {
+      var url = res.match[0];
+      var room = res.message.room;
+      var poster = res.message.user.name;
+      var current = {};
+      if (fs.existsSync(filename)) {
+        try {
+          current = jsonfile.readFileSync(filename);
+        } catch (e) {
+          console.log('fs error while getting scores');
+        }
+      }
+
+      // todo: track the most-shamed users
+      if (current[room] && current[room][url]) {
+        var repost = current[room][url];
+        if (current[room][url].poster === poster) {
+          return;
+        }
+        return res.reply(
+          ':rolleyes: ' + repost.poster + ' posted that ' + moment(repost.time).fromNow() + '.'
+        );
+      }
+
+      if (!current[room]) {
+        current[room] = {};
+      }
+      current[room][url] = {
+        poster: res.message.user.name,
+        time: Date.getTime()
+      }
+
+      jsonfile.writeFileSync(filename, current);
+    });
+
+  }
+
+}).call(this);
