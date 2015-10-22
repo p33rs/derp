bcf4d44c692226835e6edfd313801082bc7f192f
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-22 01:48:20 -0400

    whitelist blacklist

--- scripts/mlyp.js
+++ scripts/mlyp.js
@@ -1,11 +1,14 @@
 (function() {
   module.exports = function(robot) {
-    var exempt = ['ross', 'jasongriffith'];
+    var blacklist = ['ross', 'jasongriffith'];
+    var whitelist = ['sam', 'andrew', 'brendan']; // lol!
     robot.hear(/((is|are) (seriously |literally )?(bad|terrible|awful|the worst)|sucks?).?\S*$/i, function(res) {
-      if (exempt.indexOf(res.message.user.name) !== -1) {
+      if (
+        blacklist.indexOf(res.message.user.name) !== -1
+        || (whitelist.length && whitelist.indexOf(res.message.user.name) === -1) ) {
         return;
       }
-      return res.reply(':dance: much like your posting')
+      return res.reply(':dance: much like your posting');
     });
   }
 }).call(this);
