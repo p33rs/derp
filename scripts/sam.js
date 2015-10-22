c50eec995b44f3ae4a6d8850775c0071017b43b6
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-22 01:11:01 -0400

    sam.js doesnt run every time

--- scripts/sam.js
+++ scripts/sam.js
@@ -2,12 +2,13 @@
   module.exports = function(robot) {
     var timestamp = new Date();
     robot.leave(function(res) {
-        var now = new Date();
-        if (now.getTime() - timestamp.getTime() < 10000) {
-            return;
-        }
-        timestamp = now;
-        return res.send(':confuoot: hey, who was that, anyway?');
+      if (Math.random() < .75) { return; } // the joke gets old eventually
+      var now = new Date();
+      if (now.getTime() - timestamp.getTime() < 10000) {
+          return;
+      }
+      timestamp = now;
+      return res.send(':confuoot: hey, who was that, anyway?');
     });
   }
 }).call(this);
