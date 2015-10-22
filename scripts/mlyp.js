267e923aabe277b404c402bd882f51cec6efe2d8
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-22 01:38:16 -0400

    mlyp

---
+++ scripts/mlyp.js
@@ -0,0 +1,11 @@
+(function() {
+  module.exports = function(robot) {
+    var exempt = ['ross', 'jasongriffith'];
+    robot.hear(/((is|are) (seriously |literally )?(bad|terrible|awful|the worst)|sucks|suck)\S*$/i, function(res) {
+      if (exempt.indexOf(res.message.user.name) !== -1) {
+        return;
+      }
+      return res.reply(':dance: much like your posting')
+    });
+  }
+}).call(this);
