0689367e8a9e121bb75e00ec0563f0ad536b235e
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-22 02:52:39 -0400

    improved doc

--- scripts/help.js
+++ scripts/help.js
@@ -14,9 +14,8 @@
         'To create a poll, say `!vote <thing>` and then a list of options. Each option should start with a backslash. Like:',
         '`!vote Who is the best pop artist? \\Taylor Swift\\Taylor Swift\\Also Taylor Swift`',
         'Votes last ten minutes by default, but you can specify a time like this: ',
-        '`!vote +30 Question \\Answer\\Answer` (set a 30 minute timer)',
-        'You\'ll receive more info on polls when you create one.',
-        'You can also say `!roll` to roll dice. If you want, you can roll multiple dice using RPG notation: `!roll 2d12`'
+        '`!vote +30 Question \\Answer\\Answer` :arrow_left: Set a 30 minute timer',
+        'You can say `!roll` to roll dice. If you want, you can roll multiple dice using RPG notation: `!roll 2d12`'
       ];
       if (auth.isAdmin(res.message.user.name)) {
         doc.push(
@@ -25,6 +24,7 @@
           '`derp set <thing> <score>` to manually adjust the scoreboard. People will see you doing this, so don\'t abuse it.'
         );
       }
+      doc.push(':speech_balloon: Send your questions, comments, requests, and complaints to @jon, who spends too much time on Slack.');
       return robot.messageRoom.apply(
         robot,
         doc
