201629f8031d3d4684ad7dc3f03babc4e3991b2b
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-23 16:58:19 -0400

    giphy counter?

--- scripts/giphy.js
+++ scripts/giphy.js
@@ -10,9 +10,7 @@
         var jsonfile = require('jsonfile');
         var filename = '/home/jon/hubot/storage/giphy.json';
 
-        robot.hear(reg.exp('/giphy', 'i'), function(res) {
-            var user = res.message.user.name;
-            console.log(user + ' posted a giphy.');
+        var load = function() {
             var current = {}
             if (fs.existsSync(filename)) {
                 try {
@@ -21,6 +19,13 @@
                     console.log('fs error while getting giphy records');
                 }
             }
+            return current;
+        };
+
+        robot.hear(reg.exp('/giphy', 'i'), function(res) {
+            var user = res.message.user.name;
+            console.log(user + ' posted a giphy.');
+            var current = load();
             if (current[user]) {
                 current[user]++;
             } else {
@@ -31,6 +36,33 @@
                 current
             );
             return;
+        });
+
+        robot.hear(reg.exp('!giphy\s?(\S+)', 'i'), function(res) {
+          var current = load();
+          if (res.match[2]) {
+            return robot.messageRoom(
+              res.message.user.name,
+              current[res.match[2]] ? current[res.match[2]] : 0
+            );
+          } else {
+            var sorted = [];
+            for (var user in current) {
+              sorted.push([
+                user,
+                current[user]
+              ]);
+            }
+            sorted.sort(function(a, b) {
+              a = a[1];
+              b = b[1];
+              return a > b ? -1 : (a < b ? 1 : 0);
+            });
+            return robot.messageRoom(
+                res.message.user.name,
+                sorted.map(function(v) {return v[0] + ' (' + v[1] + ')';}).join(', ')
+            );
+          }
         });
 
   }
