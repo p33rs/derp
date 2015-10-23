ae57e273026bed0bfeaa55d8144893aaf23117fe
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-23 12:53:51 -0400

    figlet

--- scripts/say.js
+++ scripts/say.js
@@ -1,6 +1,7 @@
 (function() {
     module.exports = function(robot) {
 
+        var figlet = require('figlet');
         var Auth = require('../lib/auth.js');
         var auth = new Auth();
         var Regexp = require('../lib/regexp.js');
@@ -16,6 +17,24 @@
             console.log(res.message.user.name + ', to ' + dest + ': ' + msg);
             return robot.messageRoom(dest, msg);
         });
+
+      robot.hear(reg.exp('say(s|m|l|b) (\\S+) (.*)', 'i'), function(res) {
+          // must be pm from admin
+          if (!auth.isAdmin(res.message.user.name) || res.message.user.name !== res.message.room) {
+              return;
+          }
+          var dest = res.match[3];
+          var msg = res.match[4];
+          var fonts = {
+              s: 'Mini',
+              m: 'Ivrit',
+              l: 'Roman',
+              b: 'Sub-Zero'
+          };
+          console.log(res.message.user.name + ', to ' + dest + ': ' + msg);
+          var msgFormatted = '```\n' + figlet.textSync(msg, {font: fonts[res.match[2]]}) + '\n```';
+          return robot.messageRoom(dest, msgFormatted);
+      });
 
     }
 
