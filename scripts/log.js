e57f8feff4497c3cec374b8e8401c20475d338ab
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-26 18:31:00 -0400

    log fix

--- scripts/log.js
+++ scripts/log.js
@@ -1,12 +1,12 @@
 (function() {
     module.exports = function(robot) {
         var c = require('cli-color');
-        robot.receiveMiddleware = function(context, next, done) {
+        return robot.receiveMiddleware(function(context, next, done) {
             console.log(
                 c.xterm(38)(context.response.message.user.name) + ' ' + context.response.message.text
             );
             next();
-        };
+        });
     }
 
 }).call(this);
