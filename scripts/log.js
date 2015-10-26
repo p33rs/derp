b9975e496c326f059b835829bdfd24ca2ad04434
Jon Pierce <jon.pierce@sitecompli.com>
2015-10-26 19:21:30 -0400

    less enthusiastic about logging

--- scripts/log.js
+++ scripts/log.js
@@ -2,9 +2,9 @@
     module.exports = function(robot) {
         var c = require('cli-color');
         return robot.receiveMiddleware(function(context, next, done) {
-            if (context.response.message.text) {
+            if ((context.response.message.user.name === context.response.message.room) && context.response.message.text) {
                 console.log(
-                   c.xterm(38)(context.response.message.user.name) + ' ' + context.response.message.text
+                    c.xterm(38)(context.response.message.user.name) + ' ' + context.response.message.text
                 );
             }
             next(done);
