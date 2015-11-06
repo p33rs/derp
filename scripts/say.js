6f62e5b9f1695ac150b126a2098970b104e5e616
Jon Pierce <jon.pierce@sitecompli.com>
2015-11-06 11:39:43 -0500

    simplified say

--- scripts/say.js
+++ scripts/say.js
@@ -8,33 +8,37 @@
         var reg = new Regexp(robot.name);
         var c = require('cli-color');
 
-        robot.hear(reg.exp('say @?(\\S+) (.*)', 'i'), function(res) {
-            // must be pm from admin
-            if (!auth.isAdmin(res.message.user.name) || res.message.user.name !== res.message.room) {
-                return;
-            }
-            var dest = res.match[2];
-            var msg = res.match[3];
-            console.log(c.xterm(120)(res.message.user.name) + ', to ' + dest + ': ' + msg);
-            return robot.messageRoom(dest, msg);
-        });
+        var fonts = {
+            s: 'Mini',
+            m: '4Max',
+            l: 'Roman',
+            b: 'Sub-Zero'
+        };
 
-        robot.hear(reg.exp('say(s|m|l|b) @?(\\S+) (.*)', 'i'), function(res) {
+        robot.hear(reg.exp('say(s|m|l|b)? @?(\\S+) (.*)', 'i'), function(res) {
             // must be pm from admin
             if (!auth.isAdmin(res.message.user.name) || res.message.user.name !== res.message.room) {
                 return;
             }
+            var font = res.match[2];
             var dest = res.match[3];
             var msg = res.match[4];
-            var fonts = {
-                s: 'Mini',
-                m: '4Max',
-                l: 'Roman',
-                b: 'Sub-Zero'
-            };
-            console.log(c.xterm(120)(res.message.user.name) + ', to ' + dest + ' (' + fonts[res.match[2]] + '): ' + msg);
-            var msgFormatted = '```\n' + figlet.textSync(msg, {font: fonts[res.match[2]]}) + '\n```';
-            return robot.messageRoom(dest, msgFormatted);
+            var formatted = font
+                ? '```\n' + figlet.textSync(msg, {font: fonts[font]}) + '\n```'
+                : msg;
+            console.log(
+              c.xterm(120)(res.message.user.name)
+              + ', to ' + dest
+              + ' (' + ( font ? fonts[font] : 'plain' ) + '): '
+              + msg
+            );
+            try {
+                return robot.messageRoom(dest, formatted);
+            } catch (e) {
+                console.log(
+                    c.xterm(202)('error') + ' failed sending to ' + dest + '. does that user exist?'
+                );
+            }
         });
 
     }
